dbee6b0bc29bcc7718462853f2a69be192f56ece
shri-prg <155816405+shri-prg@users.noreply.github.com>
2025-09-11 15:37:04 +0530

    #

---
+++ .metadata/Operation/Example.java
@@ -0,0 +1,20 @@
+public class Example {
+    public static void main(String[] args) {
+        int a=10;
+        int b=2;
+          System.out.println(a+b);
+          System.out.println(a-b);
+          System.out.println(a*b);
+          System.out.println(a/b);
+          System.out.println(a%b);
+        double c=7.0;
+        double d=2.0;
+
+          System.out.println(c/d);
+          System.out.println(a/c);
+          System.out.println(b/d);
+          
+          
+
+    }
+}
---
+++ .metadata/Operation/Example1.java
@@ -0,0 +1,10 @@
+public class Example1{
+public static void main(String[] args) {
+    System.out.println(10+2*4+3);
+    System.out.println(10*2+4*3);
+    System.out.println(10*2/4+3);
+    System.out.println(10/2*4+3);
+    System.out.println(10%2/4*3);
+    System.out.println(10+2/4%3);
+}
+}
